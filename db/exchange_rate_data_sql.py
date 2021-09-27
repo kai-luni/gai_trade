e9d31fb5ece8340d0aa0683ff9e98d18bd435122
kluensta <kai.luenstaeden@dwd.de>
2021-09-27 15:54:22 +0200

    sqlite functionality and tests

--- db/exchange_rate_data_sql.py
+++ db/exchange_rate_data_sql.py
@@ -1,29 +1,96 @@
 
+from datetime import datetime, timedelta
 import sqlite3
 
 from ObjectsGai import ExchangeRateItem
+from CoinBaseApi import CoinBaseApi
 
 class ExchangeRateData:
-    def add_day_entry(coin : str, currency : str, day_data : ExchangeRateItem, path : str = "./"):
+    table_name = "ExchangeRateData"
+
+    def add_day_entry(day_data : ExchangeRateItem, full_path : str):
         """Add an ExchangeRateItem to db, can be empty db as well
 
         Args:
-            coin (str): coin name: BTC, ETH ..
-            currency (str): USD, EUR ...
             day_data (ExchangeRateItem): represent one day of excange rate
-            path (str): path to db, make sure to have "/" at the end
+            full_path (str): full path to db
 
         Returns:
-            bool: success
+            bool: success, false if exists already
         """
-        conn = sqlite3.connect(ExchangeRateData.getDbFullPath(coin, currency, path))
+        conn = sqlite3.connect(full_path)
         conn.execute(ExchangeRateItem.get_sqlite_create_table_query())
         conn.commit()
         conn.close()
 
-    def get_all_items(coin, currency, path):
-        conn = sqlite3.connect(ExchangeRateData.getDbFullPath(coin, currency, path))
-        cursor = conn.execute(f"SELECT {ExchangeRateItem.get_sqlite_headers()} from ExchangeRateData")
+        exchange_items = ExchangeRateData.get_all_items(full_path)
+        for i in range(len(exchange_items)):
+            item = exchange_items[i]
+            #found item to update
+            if item.date.year == day_data.date.year and item.date.month == day_data.date.month and item.date.day == day_data.date.day:
+                exchange_items[i] = day_data
+                #item exists already
+                return False
+        ExchangeRateData.insert_item(day_data, full_path)
+        return True
+        
+
+    def delete_all_items(full_path : str):
+        """Delete all entries for this table
+
+        Args:
+            full_path (str): full path to db
+        """
+        conn = sqlite3.connect(full_path)
+        # delete all rows from table
+        conn.execute(f"DELETE FROM {ExchangeRateData.table_name};")
+        conn.commit()
+        conn.close()
+
+    def fillDb(coin : str, currency : str, from_date : datetime, full_path : str):
+        """Try to fill every day in db with coin/currency values.
+
+        Args:
+            coin (str): BTC, ETH...
+            currency (str): EUR, USD...
+            from_date (datetime): start from date
+            path (str): full path to db
+
+        Returns:
+            bool: success
+        """
+
+        start =  from_date
+        end =  datetime(from_date.year, from_date.month, from_date.day, 23, 59, 59, 0)
+        while (start - timedelta(days=1)) < datetime.now():
+            data = CoinBaseApi.getDataCoinbaseDaily(f"{coin}/{currency}", start, end)
+            if len(data) == 0:
+                print(f"No entry for {start}")
+                start = start  + timedelta(days=1)
+                end = end  + timedelta(days=1)
+                continue
+            if len(data) > 1:
+                print(f"There was more than one entry for {start}")
+                start = start  + timedelta(days=1)
+                end = end  + timedelta(days=1)
+                continue
+            if not ExchangeRateData.add_day_entry(data[0], full_path):
+                return False
+            start = start  + timedelta(days=1)
+            end = end  + timedelta(days=1)
+        return True
+
+    def get_all_items(full_path):
+        """get all entries from db
+
+        Args:
+            full_path (str): path to db
+
+        Returns:
+            list[ExchangeRateItem]: entries from db
+        """
+        conn = sqlite3.connect(full_path)
+        cursor = conn.execute(f"SELECT {ExchangeRateItem.get_sqlite_headers()} from ExchangeRateData order by unix asc")
         return_list = []
         for row in cursor:
             item = ExchangeRateItem()
@@ -34,6 +101,7 @@ class ExchangeRateData:
             item.close = row[4]
             item.volume = row[5]
             item.twenty_week_average = row[6]
+            item.date = datetime.fromtimestamp(item.unix)
             return_list.append(item)
         return return_list
 
@@ -41,12 +109,20 @@ class ExchangeRateData:
         filename = f"{coin}_{currency}_echange_db.sqlite"
         return f"{path}{filename}"
 
-    def insert_item(item : ExchangeRateItem, coin, currency, path):
+    def insert_item(item : ExchangeRateItem, full_path):
         #TODO: NONE issue
-        conn = sqlite3.connect(ExchangeRateData.getDbFullPath(coin, currency, path))
+        conn = sqlite3.connect(full_path)
         conn.execute(ExchangeRateItem.get_sqlite_create_table_query())
         conn.commit()
         query = f"INSERT INTO ExchangeRateData ({ExchangeRateItem.get_sqlite_headers()}) VALUES ({item.unix}, {item.low}, {item.high}, {item.open}, {item.close}, {item.volume}, 'null' )"
         conn.execute(query)    
         conn.commit()
         conn.close()
+
+if __name__ == "__main__":
+    coin = "BTC"
+    currency = "EUR"
+    start =  datetime(2013, 8, 1, 0, 0, 0, 0)
+    full_path = ExchangeRateData.getDbFullPath(coin, currency, "")
+    ExchangeRateData.fillDb(coin, currency, start, full_path)
+
--- tests/exchange_rate_data_sql_test.py
+++ tests/exchange_rate_data_sql_test.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone
+import os
+import time
 import unittest
 
 from db.exchange_rate_data_sql import ExchangeRateData
@@ -9,6 +12,73 @@ class TestExchangeRateDataSql(unittest.TestCase):
         entry = ExchangeRateItem()
         entry.high = 1.
         entry.low = .5
-        ExchangeRateData.insert_item(entry, "BTC", "EUR", "test_db")
-        items = ExchangeRateData.get_all_items("BTC", "EUR", "test_db")
+        path_db = ExchangeRateData.getDbFullPath("BTC", "EUR", "test_db")
+        if os.path.isfile(path_db):
+            ExchangeRateData.delete_all_items(path_db)
+        ExchangeRateData.insert_item(entry, path_db)
+        items = ExchangeRateData.get_all_items(path_db)
         self.assertEqual(1, len(items))
+
+    def test_insert_two_day_entries_same_date(self):
+        entry_one, entry_two = ExchangeRateItem(), ExchangeRateItem()
+        entry_one.high = 1.
+        entry_one.low = .5
+        entry_one.unix = time.mktime(datetime(2021, 9, 26, tzinfo=timezone.utc).timetuple())
+        entry_one.date = datetime(2021, 9, 26)
+        entry_two.high = 1.
+        entry_two.low = .5
+        entry_two.unix = time.mktime(datetime(2021, 9, 26, tzinfo=timezone.utc).timetuple())
+        entry_two.date = datetime(2021, 9, 26)
+
+        path_db = ExchangeRateData.getDbFullPath("BTC", "EUR", "test_db")
+        if os.path.isfile(path_db):
+            ExchangeRateData.delete_all_items(path_db)
+
+        ExchangeRateData.add_day_entry(entry_one, path_db)
+        ExchangeRateData.add_day_entry(entry_two, path_db)
+
+        items = ExchangeRateData.get_all_items(path_db)
+        self.assertEqual(1, len(items))
+
+    def test_insert_two_day_entries_different_date(self):
+        entry_one, entry_two = ExchangeRateItem(), ExchangeRateItem()
+        entry_one.high = 1.
+        entry_one.low = .5
+        entry_one.unix = time.mktime(datetime(2021, 9, 26).timetuple())
+        entry_two.high = 1.
+        entry_two.low = .5
+        entry_two.unix = time.mktime(datetime(2021, 9, 27).timetuple())
+
+        path_db = ExchangeRateData.getDbFullPath("BTC", "EUR", "test_db")
+        if os.path.isfile(path_db):
+            ExchangeRateData.delete_all_items(path_db)
+
+        ExchangeRateData.add_day_entry(entry_one, path_db)
+        ExchangeRateData.add_day_entry(entry_two, path_db)
+
+        items = ExchangeRateData.get_all_items(path_db)
+        self.assertEqual(2, len(items))
+
+    def test_get_all_entries_in_order(self):
+        entry_one, entry_two, entry_three = ExchangeRateItem(), ExchangeRateItem(), ExchangeRateItem()
+        entry_one.high = 1.
+        entry_one.low = .5
+        entry_one.unix = time.mktime(datetime(2021, 9, 26).timetuple())
+        entry_two.high = 1.
+        entry_two.low = .7
+        entry_two.unix = time.mktime(datetime(2021, 9, 28).timetuple())
+        entry_three.high = 1.
+        entry_three.low = .6
+        entry_three.unix = time.mktime(datetime(2021, 9, 27).timetuple())
+
+        path_db = ExchangeRateData.getDbFullPath("BTC", "EUR", "test_db")
+        if os.path.isfile(path_db):
+            ExchangeRateData.delete_all_items(path_db)
+
+        ExchangeRateData.add_day_entry(entry_one, path_db)
+        ExchangeRateData.add_day_entry(entry_two, path_db)
+        ExchangeRateData.add_day_entry(entry_three, path_db)
+
+        items = ExchangeRateData.get_all_items(path_db)
+        self.assertGreater(items[1].unix, items[0].unix)
+        self.assertGreater(items[2].unix, items[1].unix)
