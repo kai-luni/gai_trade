9a2bc51432dec29ca8f5aa64e954dca8272c69b4
Kai Luenstaeden <klu@phatconsulting.de>
2023-07-07 13:56:44 +0200

    some new doc and more btc data

--- algos/RollingAverages.py
+++ algos/RollingAverages.py
@@ -1,11 +1,41 @@
 import pandas as pd
-from typing import List, Dict, Tuple
+from typing import List, Tuple
 
 class RollingAverages:
+    """
+    This class contains methods related to computing rolling averages and trading simulation based on them.
+    """
+
+    @staticmethod
     def moving_average(data: List[float], window: int) -> List[float]:
+        """
+        Calculate the moving average over a specified window for a given data set.
+
+        Parameters:
+        data (List[float]): List of floating point numbers representing the data set.
+        window (int): Size of the window over which to calculate the moving average.
+
+        Returns:
+        List[float]: List of floating point numbers representing the moving averages.
+        """
         return pd.Series(data).rolling(window=window).mean().tolist()
 
+    @staticmethod
     def get_buy_sell_signals(prices: List[float], fear_greed_index: List[int], short_window: int, long_window: int, buy_threshold: int, sell_threshold: int) -> List[Tuple[str, int]]:
+        """
+        Generate buy and sell trading signals based on moving averages and fear-greed index.
+
+        Parameters:
+        prices (List[float]): List of floating point numbers representing prices.
+        fear_greed_index (List[int]): List of integers representing the fear-greed index.
+        short_window (int): Size of the short moving average window.
+        long_window (int): Size of the long moving average window.
+        buy_threshold (int): Threshold value for the fear-greed index to trigger a buy signal.
+        sell_threshold (int): Threshold value for the fear-greed index to trigger a sell signal.
+
+        Returns:
+        List[Tuple[str, int]]: List of tuples representing buy and sell signals and their respective index positions.
+        """
         short_mavg = RollingAverages.moving_average(prices, short_window)
         long_mavg = RollingAverages.moving_average(prices, long_window)
 
@@ -24,8 +54,19 @@ class RollingAverages:
                 signals.append(("sell", i))
 
         return signals
-    
+
+    @staticmethod
     def simulate_trading(prices: List[float], signals: List[Tuple[str, int]]) -> float:
+        """
+        Simulate trading based on given buy and sell signals, starting with 1 bitcoin and $0. 
+
+        Parameters:
+        prices (List[float]): List of floating point numbers representing prices.
+        signals (List[Tuple[str, int]]): List of tuples representing buy and sell signals and their respective index positions.
+
+        Returns:
+        float: Final amount of bitcoin after executing the buy and sell signals.
+        """
         cash = 0.0
         bitcoin = 1.0
 
